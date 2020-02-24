2f9e9ffe06e18db514204d12b4507efa8c281ccd
Andres Montoya <amontoya@esvyda.com>
2020-02-23 19:27:35 -0500

    showing coords

--- src/screens/containers/map.js
+++ src/screens/containers/map.js
@@ -1,22 +1,46 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, SafeAreaView, Dimensions} from 'react-native';
+import {
+  View,
+  Alert,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
-
+navigator.geolocation = require('@react-native-community/geolocation');
+import Geolocation from '@react-native-community/geolocation';
 MapboxGL.setAccessToken(
   'pk.eyJ1IjoieWVsbG9hbGVqbyIsImEiOiJjazZ5a3dsMDgwczJnM2RydmNzcTBkaGRjIn0.J9D7xNL8jRLwm69F4ga9Og',
 );
 var {height} = Dimensions.get('window');
 
 export default class Map extends Component {
-  componentDidMount() {
-    MapboxGL.setTelemetryEnabled(false);
-  }
+  state = {
+    location: null,
+  };
+  findCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const location = JSON.stringify(position);
 
+        this.setState({location});
+      },
+      error => Alert.alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+    );
+  };
   render() {
     return (
       <SafeAreaView>
         <View style={styles.container}>
-          <MapboxGL.MapView style={styles.map} />
+          <TouchableOpacity onPress={this.findCoordinates}>
+            <Text style={styles.welcome}>Find My Coords?</Text>
+            <Text>Location: {this.state.location}</Text>
+          </TouchableOpacity>
+
+          <MapboxGL.MapView style={styles.map} centerCoordinates={[]} />
         </View>
       </SafeAreaView>
     );
